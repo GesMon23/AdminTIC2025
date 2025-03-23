841a27fc856024caa79826cf298387827210cd33
GesMon23 <100744918+GesMon23@users.noreply.github.com>
2025-03-23 15:09:55 -0600

    seguno init

--- Laboratorio1AdmonTIC/obj/Debug/net8.0/Laboratorio1AdmonTIC.AssemblyInfo.cs
+++ Laboratorio1AdmonTIC/obj/Debug/net8.0/Laboratorio1AdmonTIC.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Laboratorio1AdmonTIC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a565faf4f6b21bdc671f45521ce7c3b2a057461b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Laboratorio1AdmonTIC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Laboratorio1AdmonTIC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
